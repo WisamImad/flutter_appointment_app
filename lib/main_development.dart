69038f7bcd720b946899aabb403710d868f3304e
WisamImad <wisamimad8@gmail.com>
2025-11-05 15:09:27 +0200

    test puch

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,6 +5,7 @@ import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 import 'doc_app.dart';
 
+//05-11-2025
 void main() async{
   setupGetIt();
   // To fix texts being hidden bug in flutter_screenutil in release mode.
